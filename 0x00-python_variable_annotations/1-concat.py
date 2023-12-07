5f635a398ec2000542e308037114cf6231ac8b40
adhamessammetwally@gmail.com <adhamessammetwally@gmail.com>
2023-12-07 14:22:07 +0200

    module with string annotations

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+    Module with string anontations
+"""
+
+
+def add(str1: str, str2: str) -> str:
+    """Function that concats two strings"""
+
+    return (str1 + str2)
