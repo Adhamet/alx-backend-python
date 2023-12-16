f6fa5668d0fc80f13fb006dfd883041b1648f91e
adhamessammetwally@gmail.com <adhamessammetwally@gmail.com>
2023-12-16 16:23:52 +0200

    task 2, measure runtime

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -7,11 +7,14 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
+    """ wait_n should return the list of all the delays (float values).
+    The list of the delays should be in ascending order without using sort()
+    because of concurrency. """
     delays: List[float] = []
     sorted_delays: List[float] = []
-    for i in range(0, n):
-        delays.append(wait_random(n))
+    for i in range(n):
+        delays.append(wait_random(max_delay))
     for delay in asyncio.as_completed(delays):
-        current_delay = await delay
-        sorted_delays.append(current_delay)
+        earliest_result = await delay
+        sorted_delays.append(earliest_result)
     return sorted_delays
