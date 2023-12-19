9dfd4bc721ecfd1dae44b08ccba27d69a430981d
adhamessammetwally@gmail.com <adhamessammetwally@gmail.com>
2023-12-19 11:40:24 +0200

    task 0, async generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -5,18 +5,9 @@ import random
 from typing import Generator
 
 
-async def arange(numbers: int) -> Generator[int, None, None]:
-    """ Function that asynchronously gets the range of numbers
-    from 0 to numbers. """
-    for i in range(numbers):
-        yield i
-        await asyncio.sleep(1)
-
-
 async def async_generator() -> Generator[float, None, None]:
     """ Function that each time asynchronously waits 1 second,
     then yields a random number between 0 and 10. """
-    async for i in arange(10):
+    for i in range(0, 10):
         await asyncio.sleep(1)
-        random_number = random.uniform(0, 10)
-        yield random_number
+        yield random.uniform(0, 10)
